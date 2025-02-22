bb9e92a02ceb9dd359bc8cf5ee9d5b2e8edfee98
AiRe-flamenco <maripossa.tomoko@gmail.com>
2025-02-22 15:41:29 +0900

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// script.js
+window.addEventListener('scroll', function() {
+    var image = document.getElementById('scrollImage');
+    var scrollPosition = window.scrollY + window.innerHeight;
+    var imagePosition = image.offsetTop;
+
+    if (scrollPosition >= imagePosition) {
