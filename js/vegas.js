b52bbf8af51f8e87dac7d3416753d31efd251400
AiRe-flamenco <maripossa.tomoko@gmail.com>
2025-03-09 16:03:26 +0900

    Update vegas.js

--- js/vegas.js
+++ js/vegas.js
@@ -4,6 +4,7 @@ $(function() {
             { src: './images/1.jpg' },	//1枚目の写真指定
             { src: './images/2.jpg' },	//2枚目の写真指定
             { src: './images/3.jpg' },	//3枚目の写真指定
+	    { src: './images/4.jpg' },  //4枚目の写真指定
         ],
 		transition: 'fade',			//https://vegas.jaysalvat.com/documentation/transitions/から好みのtransitionを選んで置き換えられます。
 		timer: false,				//プログレスバーを非表示に。
